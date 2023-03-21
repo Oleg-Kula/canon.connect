eefc8aa2e42d211cd0ce85196b6caa24df8043c3
Oleg-Kula <kulacholeg@gmail.com>
2023-03-21 15:35:30 +0200

    Operations get/save

---
+++ script_sql/sql_insert_dep.sql
@@ -0,0 +1,19 @@
+insert into departments(department_code, department_name)
+values
+(7654321, 'admin'),
+(1, 'administrative'),
+(2, 'vtosi'),
+(3, '3'),
+(4, '4'),
+(5, '5'),
+(6, '6'),
+(7, '7'),
+(8, '8'),
+(9, '9'),
+(10, '10'),
+(11, '11'),
+(12, '12'),
+(13, '13'),
+(14, '14'),
+(15, '15'),
+(-1, 'unresolved')
