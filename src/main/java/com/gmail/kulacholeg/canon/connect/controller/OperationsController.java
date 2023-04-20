bae9706b8e072aa382ab7c9b2f445ac8a4b7534d
Oleg-Kula <kulacholeg@gmail.com>
2023-04-20 13:51:34 +0300

    force base update

--- src/main/java/com/gmail/kulacholeg/canon/connect/controller/OperationsController.java
+++ src/main/java/com/gmail/kulacholeg/canon/connect/controller/OperationsController.java
@@ -1,21 +1,26 @@
 package com.gmail.kulacholeg.canon.connect.controller;
 
 import com.gmail.kulacholeg.canon.connect.service.GetOperationsService;
+import com.gmail.kulacholeg.canon.connect.service.SaveOperationsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
 @RequestMapping("/operations")
 public class OperationsController {
 
-    private final GetOperationsService service;
+    private final GetOperationsService operationsService;
+    private final SaveOperationsService saveService;
+
 
     @Autowired
-    public OperationsController(GetOperationsService service) {
-        this.service = service;
+    public OperationsController(GetOperationsService operationsService, SaveOperationsService saveService) {
+        this.saveService = saveService;
+        this.operationsService = operationsService;
     }
 
     @GetMapping
@@ -27,13 +32,20 @@ public class OperationsController {
     public String  getOperationsBetweenDates(@RequestParam String from,
                                              @RequestParam String to,
                                              Model model){
-        model.addAttribute("operations", service.getOperationsBetweenDates(from, to));
+        model.addAttribute("operations", operationsService.getOperationsBetweenDates(from, to));
         return "oper";
+    }
+
+    @GetMapping("/upload")
+    public String forceUploadOperations(RedirectAttributes redirectAttributes){
+        saveService.getOperationsAndSave();
+        redirectAttributes.addFlashAttribute("message", "Базу синхронізовано!");
+        return "redirect:/operations";
     }
 
     @GetMapping("/search_day")
     public String getOperationsPerDay(@RequestParam String day, Model model){
-        model.addAttribute("operations", service.getOperationsPerDay(day));
+        model.addAttribute("operations", operationsService.getOperationsPerDay(day));
         return "oper";
     }
 }
