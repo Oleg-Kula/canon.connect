bb199c80dd5ca7d2774f58de988b0bf1b6c3063f
Oleg-Kula <kulacholeg@gmail.com>
2023-04-27 14:26:53 +0300

    pdf creation added

--- src/main/java/com/gmail/kulacholeg/canon/connect/util/PDFCreator.java
+++ src/main/java/com/gmail/kulacholeg/canon/connect/util/PDFCreator.java
@@ -1,13 +1,12 @@
 package com.gmail.kulacholeg.canon.connect.util;
 
 import com.gmail.kulacholeg.canon.connect.dto.OperationGetDto;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.pdf.PdfPTable;
-import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.*;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 public class PDFCreator {
@@ -15,11 +14,17 @@ public class PDFCreator {
     public static Document createFromList(List<OperationGetDto> operations){
         Document document = new Document();
         try {
-            PdfWriter.getInstance(document, new FileOutputStream("table.pdf"));
+            PdfWriter.getInstance(document, new FileOutputStream("table.pdf")).setInitialLeading(16);
             document.open();
             PdfPTable table = new PdfPTable(2);
-            table.addCell("Department Name");
-            table.addCell("All Operations");
+            FontSelector selector = new FontSelector();
+            BaseFont bf = BaseFont.createFont("src/main/resources/fonts/DejaVuSans.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            Font font = new Font(bf, 12);
+            selector.addFont(font);
+            PdfPCell cell = new PdfPCell(selector.process("Відділ"));
+            table.addCell(cell);
+            cell = new PdfPCell(selector.process("Кількість використаних листів"));
+            table.addCell(cell);
             for(OperationGetDto dto : operations){
                 table.addCell(dto.getDepartmentName());
                 table.addCell(dto.getAllOperations().toString());
@@ -28,8 +33,9 @@ public class PDFCreator {
             document.close();
         } catch (DocumentException | FileNotFoundException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
         return document;
     }
 }
