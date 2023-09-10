fa1c525c5f8c785240fe783d8817eb68febce31d
DESKTOP-DG21IIT\Oleh <kulacholeg@gmail.com>
2023-09-10 15:57:34 +0300

    test commit for branch

--- src/main/java/com/gmail/kulacholeg/canon/connect/controller/OperationsController.java
+++ src/main/java/com/gmail/kulacholeg/canon/connect/controller/OperationsController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-
+//rest-version
 @Controller
 @RequestMapping("/operations")
 public class OperationsController {
